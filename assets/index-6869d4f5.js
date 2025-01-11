9c5b6ea58161df150e48473d9239044f547f918a
Lesia Sasiuk <chumachocl@gmail.com>
2025-01-11 11:06:23 +0200

    Updates

--- assets/index-6869d4f5.js
+++ assets/index-6869d4f5.js
@@ -19,12 +19,16 @@
   }).observe(document, { childList: true, subtree: true });
   function getFetchOpts(link) {
     const fetchOpts = {};
-    if (link.integrity) fetchOpts.integrity = link.integrity;
-    if (link.referrerPolicy) fetchOpts.referrerPolicy = link.referrerPolicy;
+    if (link.integrity)
+      fetchOpts.integrity = link.integrity;
+    if (link.referrerPolicy)
+      fetchOpts.referrerPolicy = link.referrerPolicy;
     if (link.crossOrigin === "use-credentials")
       fetchOpts.credentials = "include";
-    else if (link.crossOrigin === "anonymous") fetchOpts.credentials = "omit";
-    else fetchOpts.credentials = "same-origin";
+    else if (link.crossOrigin === "anonymous")
+      fetchOpts.credentials = "omit";
+    else
+      fetchOpts.credentials = "same-origin";
     return fetchOpts;
   }
   function processPreload(link) {
@@ -35,6 +39,7 @@
     fetch(link.href, fetchOpts);
   }
 })();
+const main = "";
 function _toConsumableArray(arr) {
   if (Array.isArray(arr)) {
     for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) {
@@ -76,8 +81,10 @@ var preventDefault = function preventDefault2(rawEvent) {
   if (allowTouchMove(e.target)) {
     return true;
   }
-  if (e.touches.length > 1) return true;
-  if (e.preventDefault) e.preventDefault();
+  if (e.touches.length > 1)
+    return true;
+  if (e.preventDefault)
+    e.preventDefault();
   return false;
 };
 var setOverflowHidden = function setOverflowHidden2(options) {
@@ -255,7 +262,8 @@ const toggleMenu = () => {
 refsMenu.openMenuBtn.addEventListener("click", toggleMenu);
 refsMenu.closeMenuBtn.addEventListener("click", toggleMenu);
 window.matchMedia("(min-width: 1200px)").addEventListener("change", (event) => {
-  if (!event.matches) return;
+  if (!event.matches)
+    return;
   refsMenu.overlayMenu.classList.remove("is-open");
   refsMenu.openMenuBtn.setAttribute("aria-expanded", false);
   enableBodyScroll(document.body);
